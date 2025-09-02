01216cc0b1eef7efcb7acbd6e40f0d413e37a04a
Rodion <rodionb4649@gmail.com>
2025-09-02 22:07:56 +0300

    created main tab layout for timers list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonLabel } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonLabel, IonIcon, IonButton, IonButtons } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { add, pencil, trash } from 'ionicons/icons';
+import { Timer } from 'src/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
+  imports: [IonButtons, IonIcon, IonLabel, IonList, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class HomePage {
-  constructor() { }
+  timersData: Timer[] = [];
+  constructor() {
+    addIcons({ pencil, add, trash });
+  }
 }
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+export type Timer = {
+    id: number;
+    name: string;
+    exerciseDuration: number;
+    restDuration: number;
+    rounds: number;
+}
