e20c98d2358d327e92243f9149df96ac7138182f
Rodion <rodionb4649@gmail.com>
2025-09-03 21:27:59 +0300

    improved layout for timer page

--- src/app/timer/timer.page.ts
+++ src/app/timer/timer.page.ts
@@ -1,6 +1,6 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonIcon, IonButton, IonButtons, IonModal } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton, IonButtons, IonList } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { arrowBack } from 'ionicons/icons';
 import { Timer } from 'src/types';
@@ -9,7 +9,7 @@ import { Timer } from 'src/types';
   selector: 'timer-page',
   templateUrl: 'timer.page.html',
   styleUrls: ['timer.page.scss'],
-  imports: [IonButtons, IonIcon, IonLabel, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [IonList, IonButtons, IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
 })
 export class TimerPage {
   timer: Timer = { id: 0, name: "", exerciseDuration: 0, restDuration: 0, rounds: 0 };
