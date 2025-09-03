deceda2aa397bea7eeb58ed81c25373f823799f6
Rodion <rodionb4649@gmail.com>
2025-09-03 14:09:24 +0300

    created add and edit modal layout for timers

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,37 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonLabel, IonIcon, IonButton, IonButtons } from '@ionic/angular/standalone';
+import { Component, ViewChild } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonList, IonLabel, IonIcon, IonButton, IonButtons, IonModal, IonInput } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { add, pencil, trash } from 'ionicons/icons';
+import { add, pencil, trash, close } from 'ionicons/icons';
 import { Timer } from 'src/types';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonButtons, IonIcon, IonLabel, IonList, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, IonButton],
+  imports: [IonModal, IonButtons, IonIcon, IonLabel, IonList, IonItem, IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonInput],
 })
 export class HomePage {
   timersData: Timer[] = [];
+
+  @ViewChild(IonModal) addTimerModal!: IonModal;
+
+  timerTitle: string = "";
+
+  openAddTimerModal() {
+    this.timerTitle = "Add timer"
+    this.addTimerModal.present();
+  }
+
+  openEditTimerModal() {
+    this.timerTitle = "Edit timer";
+    this.addTimerModal.present();
+  }
+
+  closeModal() {
+    this.addTimerModal.dismiss();
+  }
+
   constructor() {
-    addIcons({ pencil, add, trash });
+    addIcons({ pencil, add, trash, close });
   }
 }
